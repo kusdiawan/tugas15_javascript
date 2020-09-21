485f7726075eb7045fb923d16e024f2e7e900c3e
kusdiawan <71364605+kusdiawan@users.noreply.github.com>
2020-09-21 10:24:04 +0700

    Add files via upload

---
+++ tugas15.js
@@ -0,0 +1,14 @@
+// //fungsi pow untuk menghitung pangkat
+// function panggilPow(nilai1, pangkat){
+// 	return Math.pow(nilai1, pangkat)
+// }
+// console.log(panggilPow(3,3))
+
+var ab = 8
+var bc = 6
+var ac = (Math.pow(ab, 2) + Math.pow(bc, 2))
+
+
+console.log("nilai ab : " + ab)
+console.log("nilai bc : " + bc)
+console.log("nilai ac : " + Math.sqrt(ac), "cm")
